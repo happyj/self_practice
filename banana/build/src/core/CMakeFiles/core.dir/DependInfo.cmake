38f494e6a5069eb213c74e2733680735fa036f8c
happyj <hejun0213@gmail.com>
2017-07-01 22:33:15 +0800

    makefile

---
+++ banana/build/src/core/CMakeFiles/core.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/zhangjiaqi/codes/self_practice/banana/src/core/Acceptor.cpp" "/Users/zhangjiaqi/codes/self_practice/banana/build/src/core/CMakeFiles/core.dir/Acceptor.cpp.o"
+  "/Users/zhangjiaqi/codes/self_practice/banana/src/core/Buffer.cpp" "/Users/zhangjiaqi/codes/self_practice/banana/build/src/core/CMakeFiles/core.dir/Buffer.cpp.o"
+  "/Users/zhangjiaqi/codes/self_practice/banana/src/core/Epoll.cpp" "/Users/zhangjiaqi/codes/self_practice/banana/build/src/core/CMakeFiles/core.dir/Epoll.cpp.o"
+  "/Users/zhangjiaqi/codes/self_practice/banana/src/core/EventLoop.cpp" "/Users/zhangjiaqi/codes/self_practice/banana/build/src/core/CMakeFiles/core.dir/EventLoop.cpp.o"
+  "/Users/zhangjiaqi/codes/self_practice/banana/src/core/IpAddress.cpp" "/Users/zhangjiaqi/codes/self_practice/banana/build/src/core/CMakeFiles/core.dir/IpAddress.cpp.o"
+  "/Users/zhangjiaqi/codes/self_practice/banana/src/core/IpEndPoint.cpp" "/Users/zhangjiaqi/codes/self_practice/banana/build/src/core/CMakeFiles/core.dir/IpEndPoint.cpp.o"
+  "/Users/zhangjiaqi/codes/self_practice/banana/src/core/Poll.cpp" "/Users/zhangjiaqi/codes/self_practice/banana/build/src/core/CMakeFiles/core.dir/Poll.cpp.o"
+  "/Users/zhangjiaqi/codes/self_practice/banana/src/core/Select.cpp" "/Users/zhangjiaqi/codes/self_practice/banana/build/src/core/CMakeFiles/core.dir/Select.cpp.o"
+  "/Users/zhangjiaqi/codes/self_practice/banana/src/core/Socket.cpp" "/Users/zhangjiaqi/codes/self_practice/banana/build/src/core/CMakeFiles/core.dir/Socket.cpp.o"
+  "/Users/zhangjiaqi/codes/self_practice/banana/src/core/TcpConnection.cpp" "/Users/zhangjiaqi/codes/self_practice/banana/build/src/core/CMakeFiles/core.dir/TcpConnection.cpp.o"
+  "/Users/zhangjiaqi/codes/self_practice/banana/src/core/TcpServer.cpp" "/Users/zhangjiaqi/codes/self_practice/banana/build/src/core/CMakeFiles/core.dir/TcpServer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ banana/build/src/tests/CMakeFiles/unittest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unittest.dir/Buffer_unittest.cpp.o"
+  "CMakeFiles/unittest.dir/IpAddress_unittest.cpp.o"
+  "unittest.pdb"
+  "unittest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unittest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
